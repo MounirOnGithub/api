cb7797398889f8d327b4b374693ae87be4deb008
Mounir Khanouri <mkhanouri@xee.com>
2017-07-19 03:15:33 +0200

    feat: configuration of encoder and acl, new user model and controller

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -4,12 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use FOS\RestBundle\View\ViewHandler;
-use FOS\RestBundle\View\View;
 
 /**
  * Article controller.
@@ -25,15 +20,6 @@ class ArticleController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         $articles = $em->getRepository('AppBundle:Article')->findAll();
-        foreach ($articles as $article) {
-            $formatted[] = [
-                'id' => $article->getId(),
-                'title' => $article->getTitle(),
-                'content' => $article->getContent(),
-                'createdAt' => $article->getCreatedAt(),
-                'updatedAt' => $article->getUpdatedAt(),
-            ];
-        }
 
         return $articles;
     }
---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use AppBundle\Entity\User;
+
+class UserController extends Controller
+{
+    
+}
